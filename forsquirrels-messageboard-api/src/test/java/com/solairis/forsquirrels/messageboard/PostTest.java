c128e26e59155a9f515d86596ed596f56ef9ca1b
Joshua P Johnson <joshjohnson@trulia.com>
2014-09-19 20:53:27 -0700

    Extract author and subjcet test constants

--- forsquirrels-messageboard-api/src/test/java/com/solairis/forsquirrels/messageboard/PostTest.java
+++ forsquirrels-messageboard-api/src/test/java/com/solairis/forsquirrels/messageboard/PostTest.java
@@ -8,21 +8,24 @@ import org.junit.Test;
 
 public class PostTest {
 	
+	private static final String AUTHOR = "Henry";
+	private static final String SUBJECT = "First Subject";
+	
 	private Post target;
 	
 	@Before
 	public void before_each_test() {
-		this.target = new Post("First Subject", "Henry");
+		this.target = new Post(SUBJECT, AUTHOR);
 	}
 
 	@Test
 	public void post_has_subject() {
-		assertThat(this.target.subject(), is("First Subject"));
+		assertThat(this.target.subject(), is(SUBJECT));
 	}
 	
 	@Test
 	public void post_has_author() {
 		String author = this.target.author();
-		assertThat(author, is("Henry"));
+		assertThat(author, is(AUTHOR));
 	}
 }
